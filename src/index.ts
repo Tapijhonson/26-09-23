44a295cdffc0e5f887676692e6eaf34a150dd158
Tapijhonson <moisesporto@hotmail.com>
2023-10-02 04:14:53 -0300

    Criando classe

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello world");
+class Animal {
+    nome: string;
+    idade: number;
+
+    constructor(nome: string, idade: number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+class Gato extends Animal {
+    raca: string;
+
+    constructor(nome: string, idade: number, raca: string) {
+        super(nome, idade);
+        this.raca = raca;
+
+    }
+}
+
+const meuGato = new Gato("miaumiau", 5, "siamês");
+
+console.log(`Nome: ${meuGato.nome}`);
+console.log(`Idade: ${meuGato.idade}`);
+console.log(`Raça: ${meuGato.raca}`);
